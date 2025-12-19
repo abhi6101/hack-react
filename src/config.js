fe9c39208498152ec2fa83e1858d57bff8e98063
abhi6101 <abhijain6101@gmail.com>
2025-12-19 13:30:46 +0530

    Update config for Vercel

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
-// const API_BASE_URL = "https://placement-portal-backend-nwaj.onrender.com/api";
-const API_BASE_URL = "http://localhost:8080/api";
+// Smart Config
+// If VITE_API_BASE_URL is set (in deployment), use it.
+// Otherwise, default to Localhost for development.
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api";
 
 export default API_BASE_URL;
