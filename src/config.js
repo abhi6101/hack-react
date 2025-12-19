a0c749bf56831298d940d5d7554030001662be51
abhi6101 <abhijain6101@gmail.com>
2025-12-19 12:38:34 +0530

    Switch API to Localhost for Demo

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
-const API_BASE_URL = "https://placement-portal-backend-nwaj.onrender.com/api";
+// const API_BASE_URL = "https://placement-portal-backend-nwaj.onrender.com/api";
+const API_BASE_URL = "http://localhost:8080/api";
 
 export default API_BASE_URL;
